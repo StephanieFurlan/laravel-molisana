75b55fb0cb0380abd6c8f13f3de629ccda47fe91
Stephanie <stephanie.furlan94@gmail.com>
2021-02-03 14:30:42 +0100

    Header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,19 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        hello
+        <header>
+            <img src="{{ asset('img/marchio-sito-test.png') }}" alt="">
+            <ul class="list-inline">
+                <li><a href="">Home</a></li>
+                <li class="active"><a href="">Prodotti</a></li>
+                <li><a href="">News</a></li>
+            </ul>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
     </body>
 </html>
