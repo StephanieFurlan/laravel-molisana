b0e65dc6b199d0ef7a94067b3975a1d5db04cd9a
Stephanie <stephanie.furlan94@gmail.com>
2021-02-03 17:18:27 +0100

    @include - test

---
+++ resources/views/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <img src="{{ asset('img/marchio-sito-test.png') }}" alt="">
+    <ul class="list-inline">
+        <li><a href="">Home</a></li>
+        <li class="active"><a href="">Prodotti</a></li>
+        <li><a href="">News</a></li>
+    </ul>
+</header>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,34 @@
+<main>
+    <section class="container">
+        <h2>LE LUNGHE</h2>
+        <div class="card-container">
+            @foreach ($lunghe as $pasta)
+            <div class="card">
+                <img src="{{ $pasta['src']}}" alt="">
+            </div>
+        @endforeach
+        </div>
+    </section>
+    
+    <section class="container">
+        <h2>LE CORTE</h2>
+        <div class="card-container">
+            @foreach ($corte as $pasta)
+            <div class="card">
+                <img src="{{ $pasta['src']}}" alt="">
+            </div>
+        @endforeach
+        </div>
+    </section>
+
+    <section class="container">
+        <h2>LE CORTISSIME</h2>
+        <div class="card-container">
+            @foreach ($cortissime as $pasta)
+            <div class="card">
+                <img src="{{ $pasta['src']}}" alt="">
+            </div>
+        @endforeach
+        </div>
+    </section>
+</main>
