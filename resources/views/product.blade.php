300fe4429516c8e5eef59a7e82c2400326560591
Stephanie <stephanie.furlan94@gmail.com>
2021-02-05 11:34:35 +0100

    Indexing - moving through products

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,17 +7,23 @@
             <img src="{{ $product['src-h'] }}" alt="">
             <img src="{{ $product['src-p'] }}" alt="">
             <p>{{ $product['descrizione'] }}</p>
-            
-            
-                
-            
-            <a id="chevron-left" href="">
+            @php
+                // setting previous and next product
+                $next = $id + 1;
+                if ($id == $maxId) {
+                    $next = 0;
+                }
+                $prev = $id - 1;
+                if ($id == 0) {
+                    $prev = $maxId;
+                }
+            @endphp
+            <a id="chevron-left" href="{{ route('product', ['id' => $prev])}}">
                 <i  class="fas fa-chevron-left"></i>
             </a>
-            <a id="chevron-right" href="">
+            <a id="chevron-right" href="{{ route('product', ['id' => $next])}}">
                 <i  class="fas fa-chevron-right"></i>
             </a>
-            
         </div>
     </div>
 
